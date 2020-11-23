70005d46e8ea5a2cf2b893c655b15daa3a28bd4c
babramian <brianabramian@rocketmail.com>
2020-11-22 21:59:04 -0800

    interpolating and simplified polynomials are now calculated using sympy, project is complete.

--- polynomial-finder.py
+++ polynomial-finder.py
@@ -1,15 +1,17 @@
 import numpy as np
-import sympy as sp
+from sympy import symbols, expand, factor, evaluate
 
 class interpolater():
     dt = None
+    inter_poly = None
+    simp_poly = None
 
 
     def __init__(self, data):
         self.dt = data
 
     
-    def divdif_table(self):
+    def create_divdif_table(self):
         N = len(self.dt[0])
         limit = N-1
         difference = 1
@@ -33,10 +35,10 @@ class interpolater():
     def print_div_table(self):
         N = len(self.dt[0])
 
-        print('x', end='\t')
+        print('x', end='\t\t')
         comma = ','
         for i in range(N):
-            print(f'f[{i*comma}]', end='\t')
+            print(f'f[{i}*,]', end='\t\t')
         print()
 
         for i in range(N):
@@ -44,11 +46,35 @@ class interpolater():
                 if self.dt[j][i] == None:
                     pass
                 else:
-                    print(f'{"{:.4f}".format(self.dt[j][i])}', end='\t')
+                    print(f'{"{:.3f}".format(self.dt[j][i])}', end='\t\t')
             print()
+    
 
+    def get_inter_poly(self):
+        N = len(self.dt[0])
+        x = symbols('x')
+        expr = self.dt[1][0]
+        count = 1
 
+        for i in range(2, N+1):
+            with evaluate(False):
+                next_expr = 1
 
+                for j in range(count):
+                    next_expr = next_expr * (x - self.dt[0][j])
+                next_expr = next_expr * self.dt[i][0]
+
+                expr = expr + next_expr
+                count += 1
+
+        self.inter_poly = expr
+
+        return expr
+
+    
+    def simplify(self):
+        self.simp_poly = expand(self.inter_poly)
+        return self.simp_poly
 
 
 
@@ -81,8 +107,10 @@ def get_file(filename):
 
 def main():
     buffer = interpolater(get_file('values.txt'))
-    div_table = buffer.divdif_table()
+    div_table = buffer.create_divdif_table()
     buffer.print_div_table()
+    print(f'Interpolating Polynomial is:\n {buffer.get_inter_poly().evalf(4)}')
+    print(f'Simplified Polynomial is:\n{buffer.simplify().evalf(4)}')
 
 
 
